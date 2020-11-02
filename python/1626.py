c052292537e56a280a64773b3294a7c77d16c380
littlefattiger <6698381@qq.com>
2020-11-01 19:46:47 -0800

    Create 1626.py

---
+++ python/1626.py
@@ -0,0 +1,20 @@
+#fix one position, and find longest increase subsequence
+
+class Solution:
+    def bestTeamScore(self, scores: List[int], ages: List[int]) -> int:
+        pp = []
+        for i in range(len(scores)):
+            pp.append((ages[i], scores[i]))
+        pp.sort()
+        n = len(scores)
+        dp = [0 for i in range(n )]
+        dp[0] = pp[0][1]
+        
+        for i in range(1, n):
+            dp[i] = pp[i][1]
+            for j in range(i):
+                if pp[i][1] >= pp[j][1]:
+                    dp[i] = max(dp[i], dp[j] + pp[i][1])
+                 
+        
+        return max(dp)
