52e65fc690a044b62562b1e8edc0ce3af00431e6
littlefattiger <6698381@qq.com>
2020-11-01 21:56:49 -0800

    Create 1447.py

---
+++ python/1447.py
@@ -0,0 +1,17 @@
+#https://leetcode.com/problems/simplified-fractions/
+# it is quite easy, you only need to append fraction which have gcd == 1
+
+class Solution:
+    def simplifiedFractions(self, n: int) -> List[str]:
+        res = []
+        def gcd(a,b):
+            if b > a:
+                return gcd(b,a)
+            if b == 0:
+                return a
+            return gcd(b,a%b)
+        for i in range(1, n):
+            for j in range(i + 1, n + 1):
+                if gcd(i,j) == 1:
+                    res.append(str(i) + '/' + str(j))
+        return res
