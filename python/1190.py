569a02fd0bb8889349712d0b3e8993b2068f1edb
littlefattiger <6698381@qq.com>
2020-11-03 21:20:14 -0800

    Update 1190.py

--- python/1190.py
+++ python/1190.py
@@ -1,6 +1,6 @@
 # https://leetcode.com/problems/reverse-substrings-between-each-pair-of-parentheses/
 # refer answer from lee215. Here we use a list with " is to simulate the situation, need to familar with it, add a new " at the beginning to get result, every ( has a " to store its current value
-
+# sendond solution is amazing
 
 class Solution:
     def reverseParentheses(self, s: str) -> str:
@@ -15,5 +15,28 @@ class Solution:
             else:
                 res[-1] += v
         
+        return ''.join(res)
+        
+    def reverseParentheses(self, s: str) -> str:
+        opened = []
+        pair = {}
+        for i, v in enumerate(s):
+            if v == '(':
+                opened.append(i)
+            elif v == ')':
+                j = opened.pop()
+                pair[i] = j
+                pair[j] = i
+        i = 0
+        d = 1
+        res = []
+        while i < len(s):
+            if s[i] in '()':
+                d = -d
+                i = pair[i]
+            else:
+                res.append(s[i])
+            
+            i += d
         return ''.join(res)
         
