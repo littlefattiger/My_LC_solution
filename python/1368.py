db5531370eee19ab6530e0616cacb2fe039810c8
littlefattiger <6698381@qq.com>
2020-11-09 10:25:19 -0800

    Create 1368.py

---
+++ python/1368.py
@@ -0,0 +1,31 @@
+# https://leetcode.com/problems/minimum-cost-to-make-at-least-one-valid-path-in-a-grid/
+# remember to check whether this position is visited when we process the point, maybe it has been visited by other path, then we did not need to process this point.
+# otherwise, it would TlE
+#Like 16, 17 is important
+class Solution:
+    def minCost(self, grid: List[List[int]]) -> int:
+        seen = set()
+        queue = []
+        heappush(queue,(0,0,0))
+        direction = [(0,1), (0,-1),(1,0), (-1,0)]
+        m = len(grid)
+        n = len(grid[0])
+        while queue:
+            #print(queue)
+            steps,x,y = heappop(queue)
+            if (x, y ) in seen:
+                continue
+            seen.add( (x, y))
+            if x == m - 1 and y == n - 1:
+                return steps
+            for i in range(4):
+                dx, dy = direction[i]
+                nx = x + dx
+                ny = y + dy
+                if nx < 0 or ny < 0 or nx >= m or ny >= n or (nx, ny) in seen:
+                    continue
+                if grid[x][y] == i + 1:
+                    heappush(queue,( steps, nx,ny))
+                else:
+                    heappush(queue,( steps + 1, nx,ny))
+        return - 1
