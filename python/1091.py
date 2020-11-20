9a82a577872fc9a4e3ea84e9b8660a9a887a4a4c
littlefattiger <6698381@qq.com>
2020-11-19 22:59:00 -0800

    Update 1091.py

--- python/1091.py
+++ python/1091.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/shortest-path-in-binary-matrix/
 # nothing new, sumply BFS
+# just put the  path information in the node
+
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
         m = len(grid)
@@ -11,11 +13,12 @@ class Solution:
         if grid[0][0] == 1:
             return -1
         queue = deque()
-        queue.append((0,0,1))
+        queue.append((0,0,1,'(0,1)'))
         
         while queue:
-            x,y, step = queue.popleft()
+            x,y, step,path = queue.popleft()
             if x == m -1 and y == n -1:
+                print(path)
                 return step
             
             for dx,dy in [(-1,0),(0,-1),(1,0),(0,1),(-1,-1),(1,-1),(-1,1),(1,1)]:
@@ -26,7 +29,8 @@ class Solution:
                     continue
                 if (nx,ny) in seen:
                     continue
-                queue.append((nx,ny, step + 1))
+                queue.append((nx,ny, step + 1, path + '->' +'(' + str(nx) + ',' + str(ny) + ')'))
                 seen.add((nx,ny))
             
         return -1
+        
