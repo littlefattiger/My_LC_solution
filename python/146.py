2278b2dd7e984c88c8efdf1f96db7d3e3998629b
littlefattiger <6698381@qq.com>
2020-11-19 16:29:21 -0800

    Create 146.py

---
+++ python/146.py
@@ -0,0 +1,26 @@
+
+
+
+# https://leetcode.com/problems/lru-cache/
+# orderdict solution: 
+
+from collections import OrderedDict
+
+class LRUCache:
+    def __init__(self, capacity: int):
+        self.cap = capacity
+        self.dict = OrderedDict()
+
+    def get(self, key: int) -> int:
+        if key not in self.dict:
+            return -1
+        self.dict.move_to_end(key)
+        return self.dict[key]
+
+    def put(self, key: int, value: int) -> None:
+        if key in self.dict:
+            self.dict.pop(key)
+            
+        self.dict[key] = value
+        if len(self.dict) > self.cap:
+            self.dict.popitem(last=False) 
