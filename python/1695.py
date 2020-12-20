e4cbe804126cba2bd818d80ec3dd324913bb5890
littlefattiger <6698381@qq.com>
2020-12-19 22:52:59 -0800

    Update 1695.py

--- python/1695.py
+++ python/1695.py
@@ -14,3 +14,21 @@
                 start += 1
             res = max(res, accu)
         return res
+
+       
+       #Prefix sum here
+class Solution:
+    def maximumUniqueSubarray(self, nums: List[int]) -> int:
+        res = max(nums)
+        m = defaultdict(int)
+        presum = [0] + nums
+        for i in range(len(nums)):
+            presum[i + 1] = nums[i] + presum[i]
+        last = 0
+        for i,v in enumerate(nums):
+            if v in m:
+                
+                last = max(last, m[v] + 1)
+            res = max(res,presum[i + 1] -  presum[last])
+            m[v] = i
+        return res
