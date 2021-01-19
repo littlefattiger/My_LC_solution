cbb758bf83457693dbfbc0d39132b3f8fb86850b
littlefattiger <6698381@qq.com>
2021-01-19 14:40:18 -0800

    Create state_subset.java

---
+++ java/template/state_subset.java
@@ -0,0 +1,4 @@
+for(int subset = state; subset > 0; subset = (subset-1)&state){
+                    //dp[i][state] = Math.min(dp[i][state],Math.max(dp[i - 1][state - subset],subset_time[subset] ) );
+                    //do something
+                }
