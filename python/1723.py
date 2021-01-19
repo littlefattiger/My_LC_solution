0242114e3ab5e8bf8bed6336765d4ffbe9cea866
littlefattiger <6698381@qq.com>
2021-01-19 15:46:17 -0800

    Create 1723.py

---
+++ python/1723.py
@@ -0,0 +1,47 @@
+# python need to use traditional DFS and binary search and in addition, we need to use prune, with flag = 1 to deal with symetry.
+
+class Solution:
+    def minimumTimeRequired(self, jobs: List[int], k: int) -> int:
+        n = len(jobs)
+        
+        #dp[0][0] = 0
+ 
+        jobs.sort(reverse = True)
+        def dfs( worker, limit, m):
+            if  m >= n :
+                return True
+            flag = 0
+            for j in range(k):
+                if worker[j] + jobs[m] > limit:
+                    continue
+                if worker[j] == 0:
+                    if flag:
+                        continue
+                    else:
+                        flag = 1
+                worker[j] += jobs[m]
+                if dfs( worker, limit, m + 1) :
+                    return True
+                worker[j] -= jobs[m]
+
+             
+            return False
+        
+        #print(time_subset)
+        low = 1
+        high = sum(jobs) + 1
+        
+        
+        
+        while low < high:
+            m = (low + high) // 2
+            worker = [0]* k  
+            if  dfs(worker, m, 0):
+                high = m
+            else:
+                low = m + 1
+        
+        return low
+    
+        
+        
