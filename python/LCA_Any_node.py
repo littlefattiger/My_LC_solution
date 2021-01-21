502de5ce72cc0916d3b1c61f3046201b203e5af5
littlefattiger <6698381@qq.com>
2021-01-20 23:56:06 -0800

    Update LCA_Any_node.py

--- python/LCA_Any_node.py
+++ python/LCA_Any_node.py
@@ -1,1 +1,17 @@
 # https://www.geeksforgeeks.org/least-common-ancestor-of-any-number-of-nodes-in-binary-tree/
+
+
+def findKLCA(root, keyNodes):
+	nodesSet = set(keyNodes)
+	self.res = None
+	def helper(root):
+		if not root:
+			return 0
+
+		matchNodes += helper(root.left) + helper(root.right)
+		if root.val in nodesSet:
+			matchNodes += 1
+		if matchNodes == len(nodesSet):
+			self.res = root
+		return matchNodes
+	return self.res
