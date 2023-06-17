c8d753d416e9fdb12aa8a36b1fec82f5472be3a5
littlefattiger <6698381@qq.com>
2023-06-17 15:50:57 -0700

    Update 1976. Number of Ways to Arrive at Destination.py

--- python/Topological Sort/1976. Number of Ways to Arrive at Destination.py
+++ python/Topological Sort/1976. Number of Ways to Arrive at Destination.py
@@ -1,43 +1,34 @@
-# There is an undirected weighted connected graph. You are given a positive integer n which denotes that the graph has n nodes labeled from 1 to n, and an array edges where each edges[i] = [ui, vi, weighti] denotes that there is an edge between nodes ui and vi with weight equal to weighti.
+# You are in a city that consists of n intersections numbered from 0 to n - 1 with bi-directional roads between some intersections. The inputs are generated such that you can reach any intersection from any other intersection and that there is at most one road between any two intersections.
 
-# A path from node start to node end is a sequence of nodes [z0, z1, z2, ..., zk] such that z0 = start and zk = end and there is an edge between zi and zi+1 where 0 <= i <= k-1.
+# You are given an integer n and a 2D integer array roads where roads[i] = [ui, vi, timei] means that there is a road between intersections ui and vi that takes timei minutes to travel. You want to know in how many ways you can travel from intersection 0 to intersection n - 1 in the shortest amount of time.
 
-# The distance of a path is the sum of the weights on the edges of the path. Let distanceToLastNode(x) denote the shortest distance of a path between node n and node x. A restricted path is a path that also satisfies that distanceToLastNode(zi) > distanceToLastNode(zi+1) where 0 <= i <= k-1.
-
-# Return the number of restricted paths from node 1 to node n. Since that number may be too large, return it modulo 109 + 7.
+# Return the number of ways you can arrive at your destination in the shortest amount of time. Since the answer may be large, return it modulo 109 + 7.
 
 class Solution:
-    def countRestrictedPaths(self, n: int, edges: List[List[int]]) -> int:
-        M = 10 ** 9 + 7
-        g = defaultdict(dict)
-        for u, v, w in edges:
-            g[u - 1][v-1] = w
-            g[v - 1][u-1] = w
-        shortest = [float("inf")] *n
-        shortest[-1] = 0
+    def countPaths(self, n: int, roads: List[List[int]]) -> int:
 
+
+        M = 10 ** 9 + 7
+        g = defaultdict(list)
+        for u, v, w in roads:
+            g[u].append((v, w))
+            g[v].append((u, w))
+        ways = [0] * n
+        shortest = [float("inf")] * n
+        ways[0] = 1
+        shortest[0] = 0
         heap = []
-        heapq.heappush(heap, (0, n - 1))
+        heapq.heappush(heap, (0, 0))
         while heap:
             dist, node = heappop(heap)
-            for nei in g[node]:
-                if shortest[node] + g[node][nei] < shortest[nei]:
-                    shortest[nei] = shortest[node] + g[node][nei]
+            if dist > shortest[node]:
+                continue
+            for nei, weight in g[node]:
+                if dist + weight < shortest[nei]:
+                    shortest[nei] = dist + weight
                     heapq.heappush(heap, (shortest[nei], nei))
+                    ways[nei] = ways[node]
+                elif dist + weight == shortest[nei]:
+                    ways[nei] += ways[node]
 
-        seen = set()
-        cache_dict = {}
-        def dfs(node,n):
-            if node == n -1:
-                return 1
-            if node in cache_dict:
-                return cache_dict[node]
-            count = 0
-            seen.add(node)
-            for child in g[node]:
-                if  shortest[child]< shortest[node] and child not in seen:
-                    count += dfs(child, n)
-            seen.discard(node)
-            cache_dict[node] = count
-            return count
-        return dfs(0,n)%M
+        return ways[-1]%M
